fc938da4a70a22273b960ebe81296b72e468aaf6
Jay Sokolowski <jaysoko@gmail.com>
2019-05-12 16:48:01 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -6,6 +6,10 @@ class CreateArtists < ActiveRecord::Migration
   end
 
   def change
-    create_table :artists do |t| 
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
   end
 end
